27053565400789422d33b5107f182afa2d98ed55
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-09-05 11:48:38 +0300

    Added arg parsing

---
+++ src/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string>
+
+class Settings {
+    std::string me;
+    std::string infile;
+    std::string outfile;
+    bool debug = false;
+    bool help = false;
+
+public:
+    void parse(int argc, char *argv[]);
+
+    void print_help() const;
+
+    bool process() const;
+
+    friend std::ostream &operator<<(std::ostream &os, Settings const &settings);
+};
+
+#endif //MAIN_H
