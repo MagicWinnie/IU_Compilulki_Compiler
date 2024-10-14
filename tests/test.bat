83c5f332f4a7fe4fcf567edad5c8ddfdecf9f025
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-10-14 12:10:21 +0300

    Added colors to test.bat

--- tests/test.bat
+++ tests/test.bat
@@ -4,13 +4,17 @@ setlocal enabledelayedexpansion
 set current_dir=%cd%
 echo Starting directory: %current_dir%
 
-set green=
-set red=
-set nc=
+set green=[32m
+set red=[31m
+set nc=[0m
 
 for /r "%current_dir%\examples" %%f in (*.olang) do (
     "%current_dir%\compilulki.exe" "%%f" > nul
-    if !ERRORLEVEL! == 0 (echo Successfully processed: %%f) else (echo Error processing: %%f)
+    if !ERRORLEVEL! == 0 (
+        echo !green!Successfully processed: %%f!nc!
+    ) else (
+        echo !red!Error processing: %%f!nc!
+    )
     echo =============
 )
 endlocal
