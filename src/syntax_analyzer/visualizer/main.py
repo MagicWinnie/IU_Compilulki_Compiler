874f83d1c80ab3351ad0d8bb808eaf0036cd8455
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-10-15 12:53:37 +0300

    Updated code

--- src/syntax_analyzer/visualizer/main.py
+++ src/syntax_analyzer/visualizer/main.py
@@ -22,10 +22,8 @@ def main():
     tree.create_node("Program", 0)
 
     for i in range(1, len(lines)):
-        # Calculate indentation level
         level = (len(lines[i]) - len(lines[i].lstrip(IDENTATION))) // len(IDENTATION)
 
-        # If the level is less than or equal to the current parent level, pop nodes until you find the correct parent
         while level <= parent_level:
             parent_node.pop()
             parent_level -= 1
@@ -33,14 +31,11 @@ def main():
         stripped_line = lines[i].lstrip()
         print(stripped_line, i, level, parent_level, parent_node)
 
-        # Create a new node
         tree.create_node(stripped_line, i, parent=parent_node[-1])
 
-        # If the level increased, update the parent node
         parent_level = level
         parent_node.append(i)
 
-    # Export the tree to a dot file and render it as a PNG
     tree.to_graphviz(filename="tree.dot", shape="box", reverse=True)
 
     G = pgv.AGraph("tree.dot", rankdir="TB", strict=True)
