a0e43be5fa12ea21cc95ff7e522d6d507ff190bc
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-10-29 08:10:17 +0300

    Updated test.bat to coincide with test.sh

--- tests/test.bat
+++ tests/test.bat
@@ -7,13 +7,21 @@ echo Starting directory: %current_dir%
 set green=[32m
 set red=[31m
 set nc=[0m
-
 for /r "%current_dir%\examples" %%f in (*.olang) do (
-    "%current_dir%\compilulki.exe" -g "%%f" > nul
-    if !ERRORLEVEL! == 0 (
-        echo !green!Successfully processed: %%f!nc!
-    ) else (
-        echo !red!Error processing: %%f!nc!
+    echo.%%f| find /I "\errors\" > nul && (
+      "%current_dir%\compilulki.exe" -g "%%f" > nul
+      if !ERRORLEVEL! == 0 (
+          echo !red!Successfully processed: %%f!nc!
+      ) else (
+          echo !green!Error processing: %%f!nc!
+      )
+    ) || (
+      "%current_dir%\compilulki.exe" -g "%%f" > nul
+      if !ERRORLEVEL! == 0 (
+          echo !green!Successfully processed: %%f!nc!
+      ) else (
+          echo !red!Error processing: %%f!nc!
+      )
     )
     echo =============
 )
