3ef4e92ebe044d3183f303452a6f97d15bb4c59d
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-30 20:22:50 +0300

    Fixed settings

--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -2,37 +2,48 @@
 #include <filesystem>
 #include "settings.h"
 
-void Settings::parse(const int argc, char *argv[]) {
+void Settings::parse(const int argc, char* argv[])
+{
     this->me = std::string(argv[0]);
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         // first element is our filename
         std::string opt{argv[i]};
-        if (opt.rfind("--", 0) == 0 || opt.rfind('-', 0) == 0) {
-            if (opt == "--debug" || opt == "-g") {
+        if (opt.rfind("--", 0) == 0 || opt.rfind('-', 0) == 0)
+        {
+            if (opt == "--debug" || opt == "-g")
+            {
                 this->debug = true;
-            } else if (opt == "--help" || opt == "-h") {
+            }
+            else if (opt == "--help" || opt == "-h")
+            {
                 this->help = true;
-
             }
             // Check if starts with "-O"
-            else if (opt.rfind("-O", 0) == 0) {
-                this->optimizationL_level = opt;
+            else if (opt.rfind("-O", 0) == 0)
+            {
+                this->optimization_level = opt;
             }
-            else if(opt == "--output-filename" || opt == "-o"){
-                this->output_filename = argv[++i];
+            else if (opt == "--outfile" || opt == "-o")
+            {
+                this->outfile = argv[++i];
             }
-            else {
+            else
+            {
                 std::cerr << "Unknown option: " << opt << "\n";
                 this->print_help();
 
                 std::exit(EXIT_FAILURE);
             }
-        } else {
-            if (this->infile.empty()) {
+        }
+        else
+        {
+            if (this->infile.empty())
+            {
                 this->infile = opt;
-            } else if (this->outfile.empty()) {
-                this->outfile = opt;
-            } else {
+            }
+            else
+            {
                 std::cerr << "Unknown argument: " << opt << "\n";
                 this->print_help();
 
@@ -42,29 +53,34 @@ void Settings::parse(const int argc, char *argv[]) {
     }
 }
 
-void Settings::print_help() const {
-    std::cerr << "Usage: " << this->me << " [OPTIONS] INFILE [OUTFILE]\n";
+void Settings::print_help() const
+{
+    std::cerr << "Usage: " << this->me << " [OPTIONS] INFILE\n";
     std::cerr << "Options:\n";
     std::cerr << "\t-g, --debug: Enable debugging information\n";
     std::cerr << "\t-h, --help: Show this help message\n";
     std::cerr << "\t-O<level>: Set optimization level (default: -O0)\n";
-    std::cerr << "\t-o, --output-filename: Set output filename\n";
+    std::cerr << "\t-o, --outfile: Set output filename\n";
 }
 
 
-void Settings::process() {
-    if (this->help) {
+void Settings::process()
+{
+    if (this->help)
+    {
         this->print_help();
 
         std::exit(EXIT_FAILURE);
     }
-    if (this->infile.empty()) {
+    if (this->infile.empty())
+    {
         std::cerr << "Missing mandatory parameter INFILE" << std::endl;
         this->print_help();
 
         std::exit(EXIT_FAILURE);
     }
-    if (this->outfile.empty()) {
+    if (this->outfile.empty())
+    {
         std::filesystem::path path(this->infile);
         path.replace_extension(OUTPUT_DEFAULT_EXTENSION);
         this->outfile = path.string();
@@ -72,25 +88,31 @@ void Settings::process() {
 }
 
 
-std::string Settings::get_infile() const {
+std::string Settings::get_infile() const
+{
     return this->infile;
 }
 
-bool Settings::get_debug() const {
+bool Settings::get_debug() const
+{
     return this->debug;
 }
 
-std::string Settings::get_output_filename() const {
-    return this->output_filename;
+std::string Settings::get_output_filename() const
+{
+    return this->outfile;
 }
 
-std::string Settings::get_optimization_level() const {
-    return this->optimizationL_level;
+std::string Settings::get_optimization_level() const
+{
+    return this->optimization_level;
 }
 
-std::ostream &operator<<(std::ostream &os, Settings const &settings) {
+std::ostream& operator<<(std::ostream& os, Settings const& settings)
+{
     return os << "{infile='" << settings.infile
-           << "'; outfile='" << settings.outfile
-           << "'; debug=" << settings.debug
-           << "; help=" << settings.help << "}";
+        << "'; outfile='" << settings.outfile
+        << "'; debug=" << settings.debug
+        << "; help=" << settings.help
+        << "; optimization_level=" << settings.optimization_level << "}";
 }
--- src/settings/settings.h
+++ src/settings/settings.h
@@ -4,32 +4,31 @@
 #include <string>
 #define OUTPUT_DEFAULT_EXTENSION ".o"
 
-class Settings {
+class Settings
+{
     std::string me;
     std::string infile;
     std::string outfile;
     bool debug = false;
     bool help = false;
-    std::string optimizationL_level = "-O0";
-    std::string output_filename = "output";
-
+    std::string optimization_level = "-O0";
 
 public:
-    void parse(int argc, char *argv[]);
+    void parse(int argc, char* argv[]);
 
     void print_help() const;
 
     void process();
 
-    std::string get_infile() const;
+    [[nodiscard]] std::string get_infile() const;
 
-    bool get_debug() const;
+    [[nodiscard]] bool get_debug() const;
 
-    std::string get_output_filename() const;
+    [[nodiscard]] std::string get_output_filename() const;
 
-    std::string get_optimization_level() const;
+    [[nodiscard]] std::string get_optimization_level() const;
 
-    friend std::ostream &operator<<(std::ostream &os, Settings const &settings);
+    friend std::ostream& operator<<(std::ostream& os, Settings const& settings);
 };
 
 #endif //SETTINGS_H
