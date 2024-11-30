e84aae417ccf24014ecd462ede66d37afaa64df1
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-30 14:26:01 +0300

    Fixed symbol table

--- src/semantics_analyzer/SymbolTable.cpp
+++ src/semantics_analyzer/SymbolTable.cpp
@@ -3,56 +3,66 @@
 #include "SymbolTable.h"
 
 
-void SymbolTable::addVariableEntry(const std::string &name, const std::string &type) {
+void SymbolTable::addVariableEntry(const std::string& name, const std::string& type)
+{
     varEntries[name] = {name, type};
 }
 
 
-VariableEntry *SymbolTable::lookupVariable(const std::string &name) {
+VariableEntry* SymbolTable::lookupVariable(const std::string& name)
+{
     const auto it = varEntries.find(name);
-    if (it != varEntries.end()) {
+    if (it != varEntries.end())
+    {
         return &it->second;
     }
     return nullptr; // Not found
 }
 
-std::vector<std::string> SymbolTable::getUnusedVariables() {
+std::vector<std::string> SymbolTable::getUnusedVariables()
+{
     // Find all unused variables in the current scope
     std::vector<std::string> unusedVariables;
-    for (const auto &[name, entry]: varEntries) {
-        if (!entry.is_used) {
+    for (const auto& [name, entry] : varEntries)
+    {
+        if (!entry.is_used)
+        {
             unusedVariables.push_back(name);
         }
     }
     return unusedVariables;
 }
 
-SymbolTable ScopedSymbolTable::getCurrentScope() {
+SymbolTable ScopedSymbolTable::getCurrentScope()
+{
     return scopes.back();
 }
 
 
-void ScopedSymbolTable::enterScope() {
+void ScopedSymbolTable::enterScope()
+{
     scopes.emplace_back();
 }
 
 
-void ScopedSymbolTable::leaveScope() {
-    if (!scopes.empty()) {
-
-
+void ScopedSymbolTable::leaveScope()
+{
+    if (!scopes.empty())
+    {
         scopes.pop_back();
     }
 }
 
-void ScopedSymbolTable::addVariableEntry(const std::string &name, const std::string &type, const Span &span, bool isClassField) {
-
-
-    if (!scopes.empty()) {
+void ScopedSymbolTable::addVariableEntry(const std::string& name, const std::string& type, const Span& span,
+                                         bool isClassField)
+{
+    if (!scopes.empty())
+    {
         // Use a reference to modify the actual scope on the stack
-        auto &current_scope = scopes.back();
+        auto& current_scope = scopes.back();
 
-        if (current_scope.varEntries.find(name) != current_scope.varEntries.end())  {
+        if (current_scope.varEntries.find(name) != current_scope.varEntries.end())
+        {
             throw std::runtime_error(
                 "Variable '" + name + "' is already declared in this scope " +
                 "at line: " + std::to_string(span.get_line_num()) +
@@ -65,33 +75,37 @@ void ScopedSymbolTable::addVariableEntry(const std::string &name, const std::str
         variableTypes[name] = type;
         current_scope.addVariableEntry(name, type);
         auto classEntry = lookupClass(currClassName, span, true);
-//        if(classEntry->getFieldIndex(name) != -1){
-//            throw std::runtime_error(
-//                "Variable '" + name + "' is already declared in this class " +
-//                "at line: " + std::to_string(span.get_line_num()) +
-//                " column: " + std::to_string(span.get_pos_begin())
-//            );
-//        }
-        if(isClassField){
+        //        if(classEntry->getFieldIndex(name) != -1){
+        //            throw std::runtime_error(
+        //                "Variable '" + name + "' is already declared in this class " +
+        //                "at line: " + std::to_string(span.get_line_num()) +
+        //                " column: " + std::to_string(span.get_pos_begin())
+        //            );
+        //        }
+        if (isClassField)
+        {
             VariableEntry field = {name, type};
             classEntry->addField(field);
         }
     }
 }
 
-void ScopedSymbolTable::addFunctionEntry(const std::string &name, const std::string &className,
-                                         const std::string &returnType, const Span &span,
-                                         const std::vector<std::string> &paramTypes) {
+void ScopedSymbolTable::addFunctionEntry(const std::string& name, const std::string& className,
+                                         const std::string& returnType, const Span& span,
+                                         const std::vector<std::string>& paramTypes)
+{
     std::string funcName = name;
-    for (const auto &paramType: paramTypes) {
+    for (const auto& paramType : paramTypes)
+    {
         funcName += "_" + paramType;
     }
 
     const MethodSignature signature(funcName, paramTypes);
-    ClassEntry *classEntry = lookupClass(className, span, true);
+    ClassEntry* classEntry = lookupClass(className, span, true);
     identifierTypes[name] = ID_FUNCTION;
 
-    if (classEntry->doesMethodExists(signature.methodName)) {
+    if (classEntry->doesMethodExists(signature.methodName))
+    {
         throw std::runtime_error(
             "Method '" + name + "' is already declared in this scope" +
             " at line: " + std::to_string(span.get_line_num()) +
@@ -102,15 +116,18 @@ void ScopedSymbolTable::addFunctionEntry(const std::string &name, const std::str
 }
 
 
-void ScopedSymbolTable::addFunctionValue(const std::string &name, const std::string &className,
-                                         const std::vector<std::string> &argTypes, llvm::Function *func) {
+void ScopedSymbolTable::addFunctionValue(const std::string& name, const std::string& className,
+                                         const std::vector<std::string>& argTypes, llvm::Function* func)
+{
     const MethodSignature signature(name, argTypes);
-    ClassEntry *classEntry = lookupClass(className, Span(0, 0, 0), true);
+    ClassEntry* classEntry = lookupClass(className, Span(0, 0, 0), true);
     classEntry->addMethodValue(signature, func);
 }
 
-void ScopedSymbolTable::addClassEntry(const std::string &name, const Span &span) {
-    if (classEntries.find(name) != classEntries.end()) {
+void ScopedSymbolTable::addClassEntry(const std::string& name, const Span& span)
+{
+    if (classEntries.find(name) != classEntries.end())
+    {
         throw std::runtime_error(
             "Class '" + name + "' is already declared in this scope " +
             "at line: " + std::to_string(span.get_line_num()) +
@@ -121,16 +138,20 @@ void ScopedSymbolTable::addClassEntry(const std::string &name, const Span &span)
     classEntries[name] = ClassEntry(name);
 }
 
-VariableEntry *ScopedSymbolTable::lookupVariable(const std::string &name, const Span &span,
-                                                 const bool throw_error) {
-    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) {
-        auto &[varEntries] = *it;
+VariableEntry* ScopedSymbolTable::lookupVariable(const std::string& name, const Span& span,
+                                                 const bool throw_error)
+{
+    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it)
+    {
+        auto& [varEntries] = *it;
         auto found = varEntries.find(name);
-        if (found != varEntries.end()) {
+        if (found != varEntries.end())
+        {
             return &found->second;
         }
     }
-    if (throw_error) {
+    if (throw_error)
+    {
         throw std::runtime_error(
             "Variable '" + name + "' used before declaration " +
             "at line: " + std::to_string(span.get_line_num()) +
@@ -140,31 +161,38 @@ VariableEntry *ScopedSymbolTable::lookupVariable(const std::string &name, const 
     return nullptr;
 }
 
-void ScopedSymbolTable::makeVariableUsed(const std::string &name) {
-    for (auto it = scopes.size(); it-- > 0;) {
-        auto &varEntries = scopes[it].varEntries;
+void ScopedSymbolTable::makeVariableUsed(const std::string& name)
+{
+    for (auto it = scopes.size(); it-- > 0;)
+    {
+        auto& varEntries = scopes[it].varEntries;
         auto found = varEntries.find(name);
-        if (found != varEntries.end()) {
+        if (found != varEntries.end())
+        {
             found->second.is_used = true;
             return;
         }
     }
 }
 
-MethodEntry *ScopedSymbolTable::lookupFunction(const std::string &className, const std::string &methodName,
-                                               const std::vector<std::string> &params,
-                                               const Span &span, const bool throw_error) {
+MethodEntry* ScopedSymbolTable::lookupFunction(const std::string& className, const std::string& methodName,
+                                               const std::vector<std::string>& params,
+                                               const Span& span, const bool throw_error)
+{
     auto funcName = methodName;
-    for (const auto &param: params) {
+    for (const auto& param : params)
+    {
         funcName += "_" + param;
     }
     const MethodSignature signature(funcName, params);
-    ClassEntry *classEntry = lookupClass(className, span, true);
-    if (MethodEntry *methodEntry = classEntry->lookupMethod(signature)) {
+    ClassEntry* classEntry = lookupClass(className, span, true);
+    if (MethodEntry* methodEntry = classEntry->lookupMethod(signature))
+    {
         return methodEntry;
     }
 
-    if (throw_error) {
+    if (throw_error)
+    {
         throw std::runtime_error(
             "Method '" + className + "::" + funcName + "' used before declaration " +
             "at line: " + std::to_string(span.get_line_num()) +
@@ -174,18 +202,22 @@ MethodEntry *ScopedSymbolTable::lookupFunction(const std::string &className, con
     return nullptr;
 }
 
-bool ScopedSymbolTable::doesMethodExists(std::string &name, const std::string &className) {
+bool ScopedSymbolTable::doesMethodExists(std::string& name, const std::string& className)
+{
     ClassEntry classEntry = classEntries[className];
     return classEntry.doesMethodExists(className);
 }
 
 
-ClassEntry *ScopedSymbolTable::lookupClass(const std::string &name, const Span &span,
-                                           const bool throw_error) {
-    if (classEntries.find(name) != classEntries.end()) {
+ClassEntry* ScopedSymbolTable::lookupClass(const std::string& name, const Span& span,
+                                           const bool throw_error)
+{
+    if (classEntries.find(name) != classEntries.end())
+    {
         return &classEntries[name];
     }
-    if (throw_error) {
+    if (throw_error)
+    {
         throw std::runtime_error(
             "Class '" + name + "' used before declaration " +
             "at line: " + std::to_string(span.get_line_num()) +
@@ -195,82 +227,94 @@ ClassEntry *ScopedSymbolTable::lookupClass(const std::string &name, const Span &
     return nullptr;
 }
 
-IdentifierType ScopedSymbolTable::getIdentifierType(const std::string &identifier) {
-    if (identifierTypes.find(identifier) != identifierTypes.end()) {
+IdentifierType ScopedSymbolTable::getIdentifierType(const std::string& identifier)
+{
+    if (identifierTypes.find(identifier) != identifierTypes.end())
+    {
         return identifierTypes[identifier];
     }
     return ID_UNDEFINED;
 }
 
-llvm::Value *ScopedSymbolTable::getLocalVariable(const std::string &varName) {
+llvm::Value* ScopedSymbolTable::getLocalVariable(const std::string& varName)
+{
     return varEntries[varName];
 }
 
-void ScopedSymbolTable::addLocalVariable(const std::string &varName, llvm::Value *pInst, const std::string &type) {
+void ScopedSymbolTable::addLocalVariable(const std::string& varName, llvm::Value* pInst, const std::string& type)
+{
     varEntries[varName] = pInst;
 }
 
 
-std::string ScopedSymbolTable::getFunctionType(const std::string &name, const std::string &className) {
+std::string ScopedSymbolTable::getFunctionType(const std::string& name, const std::string& className)
+{
     const auto classEntry = lookupClass(className, Span(0, 0, 0), false);
     return classEntry->getMethodReturnType(name);
 }
 
-std::string ScopedSymbolTable::getIdentifierStringType(const std::string &identifier, const std::string &className,
-                                                       const Span &span) {
-    switch (getIdentifierType(identifier)) {
-        case ID_VARIABLE:
-            return variableTypes[identifier];
-        case ID_FUNCTION:
-            if (className == "void") {
-                throw std::runtime_error(
-                    "void has no method " + identifier +
-                    " at line: " + std::to_string(span.get_line_num()) +
-                    " column: " + std::to_string(span.get_pos_begin())
-                );
-            }
-            return getFunctionType(identifier, className);
-        default:
+std::string ScopedSymbolTable::getIdentifierStringType(const std::string& identifier, const std::string& className,
+                                                       const Span& span)
+{
+    switch (getIdentifierType(identifier))
+    {
+    case ID_VARIABLE:
+        return variableTypes[identifier];
+    case ID_FUNCTION:
+        if (className == "void")
+        {
             throw std::runtime_error(
-                "Identifier " + identifier + " is not declared" +
+                "void has no method " + identifier +
                 " at line: " + std::to_string(span.get_line_num()) +
                 " column: " + std::to_string(span.get_pos_begin())
             );
+        }
+        return getFunctionType(identifier, className);
+    default:
+        throw std::runtime_error(
+            "Identifier " + identifier + " is not declared" +
+            " at line: " + std::to_string(span.get_line_num()) +
+            " column: " + std::to_string(span.get_pos_begin())
+        );
     }
 }
 
-std::string ScopedSymbolTable::getIdentifierStringType(const std::string &identifier, const Span &span) {
-    switch (getIdentifierType(identifier)) {
-        case ID_VARIABLE:
-            return variableTypes[identifier];
-        case ID_CLASS:
-            return identifier;
-        default:
-            throw std::runtime_error(
-                "Identifier " + identifier + " is not declared" +
-                " at line: " + std::to_string(span.get_line_num()) +
-                " column: " + std::to_string(span.get_pos_begin())
-            );
+std::string ScopedSymbolTable::getIdentifierStringType(const std::string& identifier, const Span& span)
+{
+    switch (getIdentifierType(identifier))
+    {
+    case ID_VARIABLE:
+        return variableTypes[identifier];
+    case ID_CLASS:
+        return identifier;
+    default:
+        throw std::runtime_error(
+            "Identifier " + identifier + " is not declared" +
+            " at line: " + std::to_string(span.get_line_num()) +
+            " column: " + std::to_string(span.get_pos_begin())
+        );
     }
 }
 
-llvm::Function *ScopedSymbolTable::getMethodValue(const std::string &className, const std::string &funcName,
-                                                  const std::vector<std::string> &argTypes) {
+llvm::Function* ScopedSymbolTable::getMethodValue(const std::string& className, const std::string& funcName,
+                                                  const std::vector<std::string>& argTypes)
+{
     auto classEntry = lookupClass(className, Span(0, 0, 0), false);
     return classEntry->getMethodValue(funcName, argTypes);
 }
 
-int ScopedSymbolTable::getFieldIndex(std::string className, std::string varName) {
-    auto classEntry = lookupClass(className, Span(0, 0, 0), false);
+int ScopedSymbolTable::getFieldIndex(const std::string& className, const std::string& varName)
+{
+    const auto classEntry = lookupClass(className, Span(0, 0, 0), false);
     return classEntry->getFieldIndex(varName);
 }
 
-llvm::Value *ScopedSymbolTable::getThisPointer() {
+llvm::Value* ScopedSymbolTable::getThisPointer() const
+{
     return thisPtr;
 }
 
-void ScopedSymbolTable::setThisPointer(llvm::Value *thisPtr) {
+void ScopedSymbolTable::setThisPointer(llvm::Value* thisPtr)
+{
     this->thisPtr = thisPtr;
 }
-
-
